0cc734e7546f3e583a7e2e6be7aa1a0019767aae
KenChim <giaanhnp@gmail.com>
2026-04-14 18:20:39 +0700

    Added store logic and tested the store

---
+++ Lab03/Store.java
@@ -0,0 +1,49 @@
+package Lab03;
+
+public class Store {
+    private static final int MAX_NUMBER_ITEMS = 40;
+    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[MAX_NUMBER_ITEMS];
+    public int numberItems = 0;
+    public Store(){
+
+    }
+
+    public void addDVD(DigitalVideoDisc d){
+        if(numberItems == MAX_NUMBER_ITEMS){
+            System.out.println("Too many items already!");
+        }
+        itemsInStore[numberItems++] = d;
+    }
+
+    public void removeDVDbyTitle(String title){
+        int del = 0;
+        int songhanh[] = new int[40];
+        DigitalVideoDisc temp[] = new DigitalVideoDisc[40];
+        for(int i = 0; i<numberItems; i++){
+            songhanh[i] = 0;
+            if((itemsInStore[i].getTitle()).equals(title)){
+                songhanh[i] = -1;
+                del++;
+            }
+            temp[i] = itemsInStore[i];
+        }
+        int count1 = 0;
+        for(int i = 0; i<numberItems; i++){
+            if(songhanh[i] == -1) continue;
+            else itemsInStore[count1++] = temp[i];
+        }
+        if(del == 0){
+            System.out.println("Khong tim thay dvd nao nhu vay");
+        }
+        numberItems -= del;
+    }
+    public void printStore(){
+        System.out.println("*******************************STIORE******************************");
+        System.out.println("Ordered items: ");
+        for(int i =0; i<numberItems; i++){
+            DigitalVideoDisc d = itemsInStore[i];
+            System.out.printf("ID: %d. DVD - %s - %s - %s - %d : %.2f\n",d.getId(), d.getTitle(), d.getCategory(), d.getDirector(), d.getLength(), d.getCost());
+        }
+        System.out.println("*******************************************************************");
+    }
+}
---
+++ Lab03/StoreTest.java
@@ -0,0 +1,28 @@
+package Lab03;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("5cm/s",
+            "Anime", "Makoto Shinkai", 97, 19.95f);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("TenSura",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Asagao to Kase-san",
+                "Yuri", "Murasaki", 88,  18.99f);
+        
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("MarriageToxin",
+                "Romance-Comedy", "Murasaki", 82,  18.99f);
+
+        Store store = new Store();
+
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        store.addDVD(dvd4);
+
+        store.removeDVDbyTitle("TenSura");
+
+        store.printStore();
+    }
+}
