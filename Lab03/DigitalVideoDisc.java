63189056e613b6b05d2c137576d224d613134919
KenChim <giaanhnp@gmail.com>
2026-04-14 14:19:20 +0700

    Class members

--- Lab03/DigitalVideoDisc.java
+++ Lab03/DigitalVideoDisc.java
@@ -5,22 +5,28 @@ public class DigitalVideoDisc{
     private String director;
     private int length;
     private float cost;
-    
-    
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title) {
         super();
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -28,6 +34,8 @@ public class DigitalVideoDisc{
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public String getTitle() {
         return title;
